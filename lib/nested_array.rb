92121e567267fa97c9c0e4ad5773c566c86f921d
Eli Landau <eli.b.landau@gmail.com>
2020-06-06 18:02:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -24,7 +24,8 @@ def assembled_matrix
   
   [
     ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
-    ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
+    ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus" 
+]
  ]
 end
 
